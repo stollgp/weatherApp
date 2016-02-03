3f1b150fe0e31571259a96a45a336d11d952a4c7
Gary Stoll <stollgp@Garys-MacBook-Pro.local>
2016-02-02 18:13:11 -0600

    first project commit

---
+++ weatherApp/Weather.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  weatherApp
+//
+//  Created by Gary Stoll on 1/21/16.
+//  Copyright © 2016 StollenSoft. All rights reserved.
+//
+
+import Foundation
+
+class Weather {
+    private var _currTemp: String!
+    private var _city: String!
+    private var _state: String!
+    private var _lowTemp: String!
+    private var _highTemp: String!
+}
